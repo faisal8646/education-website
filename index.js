44f9ef80bb5bad902818788f4277db28c7682322
faisal8646 <fa996879@gmail.com>
2023-03-03 00:16:31 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+function sendMail() {
+    var params = {
+        name:  document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_ls3zsb9";
+const templateID = "template_6byk3so"
+
+emailjs.send(serviceID,templateID,params)
+.then(
+    res =>{
+        name:  document.getElementById("name").value = "";
+        email: document.getElementById("email").value = "";
+        subject: document.getElementById("subject").value = "";
+        message: document.getElementById("message").value = "";
+        console.log(res);
+        alert("your message sent successfully");
+    }
+)
+.catch((err) => console.log (err));
+}
+
+
